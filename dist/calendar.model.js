c23e2b5e545f8f04faf91b5984859caaa10188d8
Jorge Sanchez Garrido <jsanchez@MaiTai.local>
2017-11-30 09:52:19 +0100

    Dist added

---
+++ dist/calendar.model.js
@@ -0,0 +1,25 @@
+var CalendarMonth = /** @class */ (function () {
+    function CalendarMonth() {
+    }
+    return CalendarMonth;
+}());
+export { CalendarMonth };
+var YearPicker = /** @class */ (function () {
+    function YearPicker() {
+    }
+    return YearPicker;
+}());
+export { YearPicker };
+var PrivateCalendarOptions = /** @class */ (function () {
+    function PrivateCalendarOptions() {
+    }
+    return PrivateCalendarOptions;
+}());
+export { PrivateCalendarOptions };
+var CalendarResult = /** @class */ (function () {
+    function CalendarResult() {
+    }
+    return CalendarResult;
+}());
+export { CalendarResult };
+//# sourceMappingURL=calendar.model.js.map
